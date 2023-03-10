c8cf673a70e7d18600e477e0b2e32c0f5200c471
Ahmed Kasu <ankassu@gmail.com>
2023-03-10 11:25:40 +0200

    Updated dependencies and modified login validation

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Joi = require("joi");
-const usersStore = require("../store/users");
-const validateWith = require("../middleware/validation");
+const Joi = require('joi');
+const usersStore = require('../store/users');
+const validateWith = require('../middleware/validation');
+
+const pattern = '^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$';
 
 const schema = {
   name: Joi.string().required().min(2),
   email: Joi.string().email().required(),
-  password: Joi.string().required().min(5),
+  password: Joi.string().regex(RegExp(pattern)).required().min(8),
 };
 
-router.post("/", validateWith(schema), (req, res) => {
+router.post('/', validateWith(schema), (req, res) => {
   const { name, email, password } = req.body;
   if (usersStore.getUserByEmail(email))
     return res
       .status(400)
-      .send({ error: "A user with the given email already exists." });
+      .send({ error: 'A user with the given email already exists.' });
 
   const user = { name, email, password };
   usersStore.addUser(user);
@@ -23,7 +25,7 @@ router.post("/", validateWith(schema), (req, res) => {
   res.status(201).send(user);
 });
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.send(usersStore.getUsers());
 });
 
